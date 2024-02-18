659f734b95a8556b68e4c7d9bb9c38814e85d510
yagcifuad <104993766+yagcifuad@users.noreply.github.com>
2024-02-18 23:51:20 +0200

    Add initial project files

---
+++ src/Tree.java
@@ -0,0 +1,10 @@
+public interface Tree<E> extends Iterable<E> {
+    public boolean insert(E e);
+    public boolean delete(E e);
+    public boolean search(E e);
+    public void inorder();
+    public void postorder();
+    public void preorder();
+    public int getSize();
+    public boolean isEmpty();
+}
